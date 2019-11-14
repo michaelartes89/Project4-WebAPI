b74f5aad6c390bb7cf2fca95e4f235faf8d9db85
MDBA <artes.mike@gmail.com>
2019-11-14 14:54:57 -0500

    added function to check user answer for correct responce

--- Assets/script.js
+++ Assets/script.js
@@ -1,140 +1,184 @@
 //wait until page is loaded
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
-//create variables to target DOM elements
+    //create variables to target DOM elements
 
-var startBtn = document.getElementById("startButton");
-var startCard = document.getElementById("startingCard");
-var questCard = document.getElementById("questionCard");
-var timerDisplay = document.getElementById("timer");
-var alert = document.getElementById("alert");
-var footer = document.getElementById("footer");
-var scoreLink = document.getElementById("scoreLink");
-var scoreCard = document.getElementById("scoreCard");
-var highScoreList = document.getElementById("highScoreList");
-var instructions = document.getElementById("instructions");
+    var startBtn = document.getElementById("startButton");
+    var startCard = document.getElementById("startingCard");
+    var questCard = document.getElementById("questionCard");
+    var timerDisplay = document.getElementById("timer");
+    var alert = document.getElementById("alert");
+    var footer = document.getElementById("footer");
+    var scoreLink = document.getElementById("scoreLink");
+    var scoreCard = document.getElementById("scoreCard");
+    var highScoreList = document.getElementById("highScoreList");
+    var instructions = document.getElementById("instructions");
 
-var questText = document.getElementById("questionPromt");
-var clearBtn = document.getElementById("clearBtn");
-var clearBTnArea = document.getElementById("clearBtnArea");
+    var questText = document.getElementById("questionPromt");
+    var clearBtn = document.getElementById("clearBtn");
+    var clearBTnArea = document.getElementById("clearBtnArea");
 
 
-//answer input
-var a = document.getElementById("AAnwers");
-var b = document.getElementById("BAnwers");
-var c = document.getElementById("CAnwers");
-var d = document.getElementById("DAnwers");
+    //answer input
+    var a = document.getElementById("AAnwers");
+    var b = document.getElementById("BAnwers");
+    var c = document.getElementById("CAnwers");
+    var d = document.getElementById("DAnwers");
 
-//answer buttons
-var aBtn = document.getElementById("AanswerBtn");
-var bBtn = document.getElementById("BanswerBtn");
-var cBtn = document.getElementById("DanswerBtn");
-var dBtn = document.getElementById("DanswerBtn");
+    //answer buttons
+    var aBtn = document.getElementById("AanswerBtn");
+    var bBtn = document.getElementById("BanswerBtn");
+    var cBtn = document.getElementById("DanswerBtn");
+    var dBtn = document.getElementById("DanswerBtn");
 
-var probelmTime = 15;
-var penalty = 5; 
+    var probelmTime = 15;
+    var penalty = 5;
 
-var iter = 0;
-var timer = 75;
-var endTime = 0;
-var correct = 0;
-var wrong = 0;
-var user = "";
-var score = 0;
-var newQuestions = questions;
-var scoreList = [];
-var maxScore = 0;
-var scoreshown = false;
+    var iter = 0;
+    var timer = 75;
+    var endTime = 0;
+    var correct = 0;
+    var wrong = 0;
+    var user = "";
+    var score = 0;
+    var newQuestions = questions;
+    var scoreList = [];
+    var maxScore = 0;
+    var scoreshown = false;
 
-instructions.textContent = ("Your score is your number of correct ansers times "+problemTime+ "plus the time remaning. When you get a question wrong you lose" +penalty+ "seconds from your time");
+    instructions.textContent = ("Your score is your number of correct ansers times " + problemTime + "plus the time remaning. When you get a question wrong you lose" + penalty + "seconds from your time");
 
-// randomize array elements oder in place using Durstenfeld shuffle algorith (as seen in Alex's repo)
+    // randomize array elements oder in place using Durstenfeld shuffle algorith (as seen in Alex's repo)
 
-function shuffle (array) {
-    var currentIndex = array.length, temporaryValue, randonIndex;
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randonIndex;
 
-    while(0 !== currentIndex){
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
     }
-    return array;
-}
 
     //resets card to the next quetions 
-function setQuestions(index) {
-    questText.textContent = question[index].title;
-    let choiceArray = questions[index].choices
-    choiceArray = shuffle(choiceArray)
-    
-    a.textContent = choiceArray[0];
-    b.textContent = choiceArray[1];
-    c.textContent = choiceArray[2];
-    d.textContent = choiceArray[3];
+    function setQuestions(index) {
+        questText.textContent = question[index].title;
+        let choiceArray = questions[index].choices
+        choiceArray = shuffle(choiceArray)
 
-}
-//adding eventlisteners to buttons
+        a.textContent = choiceArray[0];
+        b.textContent = choiceArray[1];
+        c.textContent = choiceArray[2];
+        d.textContent = choiceArray[3];
 
-startBtn.addEventListener("click", function () {
-    startCard.style.display = "none";
-    questCard.style.display = "block";
-    beginGame();
-});
-
-// see scorelist
-
-scoreLink.addEventListener("click", function() {
-    event.preventDefault();
-    if (scoreShown) {
-        scoreShown =false;
-        scoreCard.style.display = "none";
-    } else {
-        scoreShow = true;
-        scoreCard.style.display ="block";
     }
-    scoreSet();
-});
+    //adding eventlisteners to buttons
 
-scoreCard.style.display = "none"; //set score to hidden by default 
+    startBtn.addEventListener("click", function () {
+        startCard.style.display = "none";
+        questCard.style.display = "block";
+        beginGame();
+    });
 
-//clear saved scores
-clearBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    clearScores();
+    // see scorelist
 
-});
-
-//assigns listeners to answer buttons
-var addBtns = document.getElementsByClassName("asnwerBtn");
-for (var i = 0; i < addBtns.length; i++) {
-    addBtns[i].addEventListener("click", userChoice, false);
-}
-
-
-
-//starts the game 
-function beginGame() {
-    correct = 0;
-    wrong = 0;
-    iter = 0;
-    timer = questions.length * probelmTime; 
-
-    setQuestions(iter);
-
-    interval = setInterval(function() {
-
-        timerDisp.textContent = timer;
-        timer--;
-        
-        if (timer <0) {
-            endgame();
-        } else if (iter >= newQuestions.length) {
-            endGame();
+    scoreLink.addEventListener("click", function () {
+        event.preventDefault();
+        if (scoreShown) {
+            scoreShown = false;
+            scoreCard.style.display = "none";
+        } else {
+            scoreShow = true;
+            scoreCard.style.display = "block";
         }
-    },1000);
+        scoreSet();
+    });
+
+    scoreCard.style.display = "none"; //set score to hidden by default 
+
+    //clear saved scores
+    clearBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        clearScores();
 
     });
+
+    //assigns listeners to answer buttons
+    var addBtns = document.getElementsByClassName("asnwerBtn");
+    for (var i = 0; i < addBtns.length; i++) {
+        addBtns[i].addEventListener("click", userChoice, false);
+    }
+
+    //run scoreSet in case there is saved data
+    scoreSet();
+
+    // this function checks to see if the answer the user selects is correct
+    function userChoice(event) {
+        event.preventDefault();
+
+        let userAnwer = "";
+        userAnwer = event.target.nextElementSibling.textContent;
+
+        if (userAnwer === newQuestions[iter].anwser) {
+            console.log("win");
+            console.log(iter);
+            correct++;
+            footer.textContent = "Right!"
+
+        } else {
+            console.log("lose");
+            console.log("iter");
+            timer -= penalty;
+            wrong++;
+            if (wrong > 1) {
+                questCard.classList.remove("shake");
+                void questCard.offsetWidthl
+                questCard.classList.add("shake");
+
+
+            } else {
+                questCard.classList.add("shake");
+                footer.textContent = "Wrong!"
+            }
+            if (iter < (newQuestions.length - 1)) {
+                iter++;
+                setQuestions(iter);
+
+            } else if (iter === (newQUestions.length - 1)) {
+                iter++;
+            }
+
+
+        }
+
+
+
+
+
+        //starts the game 
+        function beginGame() {
+            correct = 0;
+            wrong = 0;
+            iter = 0;
+            timer = questions.length * probelmTime;
+
+            setQuestions(iter);
+
+            interval = setInterval(function () {
+
+                timerDisp.textContent = timer;
+                timer--;
+
+                if (timer < 0) {
+                    endgame();
+                } else if (iter >= newQuestions.length) {
+                    endGame();
+                }
+            }, 1000);
+
+        });
 
